e3a80b0515d5055be1c6e83498cfb4bf51e51b58
nightcore <nightcore@hehe.com>
2025-11-30 04:12:34 +0530

    home-manager - use xdg dirs

--- home-manager-gnome.nix
+++ home-manager-gnome.nix
@@ -1,9 +1,28 @@
-# home-manager/sayantan.nix
-{ config, pkgs, ... }:
+{ config, pkgs, username, ... }:
 
 {
+  system.activationScripts.postSwitch = {
+    text = ''
+      rm /home/${username}/.config/user-dirs.dirs
+    '';
+  };
+
   home-manager.users.nightcore = {
     home.stateVersion = "25.05";
+    
+    xdg.userDirs = {
+      enable = true;
+      createDirectories = true;
+
+      desktop        = "${config.users.users.${username}.home}/Desktop";
+      documents      = "${config.users.users.${username}.home}/Documents";
+      download       = "${config.users.users.${username}.home}/Downloads";
+      music          = "${config.users.users.${username}.home}/Music";
+      pictures       = "${config.users.users.${username}.home}/Pictures";
+      publicShare    = "${config.users.users.${username}.home}/Public";
+      templates      = "${config.users.users.${username}.home}/Templates";
+      videos         = "${config.users.users.${username}.home}/Videos";
+    };
 
     home.packages = with pkgs; [
       htop
